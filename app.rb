dcd23af20d9cf4182a7a0999540d8ea0f5ec0b8a
Franerial <brugunt@gmail.com>
2021-07-04 02:29:44 +0300

    app fix

--- app.rb
+++ app.rb
@@ -1,19 +1,51 @@
 # frozen_string_literal: true
 
-require_relative 'services/time_converter'
+require_relative "services/time_converter"
 
 class App
+  AVAILABLE_PATH = "/time"
+  AVAILABLE_PARAM = "format"
+  ACCEPTABLE_FORMAT = %w[year month day hour minute second].freeze
+
   def call(env)
     @request = Rack::Request.new(env)
-    @response = Rack::Response.new(*create_response_data)
+    @response = Rack::Response.new(body, status)
 
     @response.finish
   end
 
   private
 
-  def create_response_data
-    converter = TimeConverter.new(@request)
-    converter.call
+  def status
+    if @request.path_info == AVAILABLE_PATH && acceptably? && @request.get?
+      200
+    elsif @request.path_info == AVAILABLE_PATH && @request.get? && correct_param_name?
+      400
+    else
+      404
+    end
+  end
+
+  def body
+    if @request.path_info == AVAILABLE_PATH && acceptably? && @request.get?
+      result = TimeConverter.new(@request).call
+      result.success? ? result.data : ["Not found"]
+    elsif @request.path_info == AVAILABLE_PATH && correct_param_name? && @request.get?
+      ["Unknown time format #{unknown_time_format}"]
+    else
+      ["Not found"]
+    end
+  end
+
+  def acceptably?
+    (@request.params["format"].split(",") - ACCEPTABLE_FORMAT).empty? if correct_param_name?
+  end
+
+  def correct_param_name?
+    @request.params.keys.first == AVAILABLE_PARAM
+  end
+
+  def unknown_time_format
+    @request.params["format"].split(",") - ACCEPTABLE_FORMAT
   end
 end
--- services/time_converter.rb
+++ services/time_converter.rb
@@ -1,63 +1,33 @@
 # frozen_string_literal: true
 
 class TimeConverter
-  AVAILABLE_PATH = '/time'
-  AVAILABLE_PARAM = 'format'
-  ACCEPTABLE_FORMAT = %w[year month day hour minute second].freeze
   TIME_CONVERT_FORMATS = {
-    'year' => '%Y',
-    'month' => '%m',
-    'day' => '%d',
-    'hour' => '%H',
-    'minute' => '%M',
-    'second' => '%S'
+    "year" => "%Y",
+    "month" => "%m",
+    "day" => "%d",
+    "hour" => "%H",
+    "minute" => "%M",
+    "second" => "%S",
   }.freeze
+
+  Result = Struct.new(:data) do
+    def success?
+      !data.nil?
+    end
+  end
 
   def initialize(request)
     @request = request
   end
 
   def call
-    [body, status]
-  end
-
-  def status
-    if @request.path_info == AVAILABLE_PATH && acceptably? && @request.get?
-      200
-    elsif @request.path_info == AVAILABLE_PATH && @request.get?
-      400
+    if @request.params["format"]
+      display_format = @request.params["format"].split(",").reduce("") do |sum, param|
+        "#{sum}#{TIME_CONVERT_FORMATS[param]}-"
+      end.chop
+      Result.new(Time.now.strftime(display_format))
     else
-      404
+      Result.new
     end
-  end
-
-  def body
-    if @request.path_info == AVAILABLE_PATH && correct_param_name? && acceptably? && @request.get?
-      [convert_user_format]
-    elsif @request.path_info == AVAILABLE_PATH && correct_param_name? && @request.get?
-      ["Unknown time format #{unknown_time_format}"]
-    else
-      ['Not found']
-    end
-  end
-
-  def acceptably?
-    (@request.params['format'].split(',') - ACCEPTABLE_FORMAT).empty? if @request.params['format']
-  end
-
-  def correct_param_name?
-    @request.params.keys.first == AVAILABLE_PARAM
-  end
-
-  def unknown_time_format
-    @request.params['format'].split(',') - ACCEPTABLE_FORMAT
-  end
-
-  def convert_user_format
-    display_format = @request.params['format'].split(',').reduce('') do |sum, param|
-      "#{sum}#{TIME_CONVERT_FORMATS[param]}-"
-    end.chop
-
-    Time.now.strftime(display_format)
   end
 end
