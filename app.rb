c6cb695d01110a9ece441fd2fc7c4dfd0e8aad3e
Franerial <brugunt@gmail.com>
2021-07-07 20:36:44 +0300

    app fix 4

--- app.rb
+++ app.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-require_relative "services/time_converter"
+require_relative 'services/time_converter'
 
 class App
   def call(env)
     @request = Rack::Request.new(env)
 
-    result = TimeConverter.new(@request).call
-    if result.success?
-      @response = Rack::Response.new(result.body, 200)
-    else
-      @response = Rack::Response.new(result.body, 400)
-    end
+    result = TimeConverter.new(@request.params['format']).call
+    @response = if result.success?
+                  Rack::Response.new(result.body, 200)
+                else
+                  Rack::Response.new(result.body, 400)
+                end
 
     @response.finish
   end
--- services/time_converter.rb
+++ services/time_converter.rb
@@ -1,50 +1,47 @@
 # frozen_string_literal: true
 
 class TimeConverter
-  ACCEPTABLE_FORMAT = %w[year month day hour minute second].freeze
-  AVAILABLE_TIME_FORMATS = %w[%Y %m %d %H %M %S].freeze
-  TIME_CONVERT_FORMATS = Hash[ACCEPTABLE_FORMAT.zip AVAILABLE_TIME_FORMATS].freeze
+  TIME_CONVERT_FORMATS = {
+    'year' => '%Y',
+    'month' => '%m',
+    'day' => '%d',
+    'hour' => '%H',
+    'minute' => '%M',
+    'second' => '%S'
+  }.freeze
 
   Result = Struct.new(:body) do
     def success?
-      body.first.include? "-"
+      body.include? '-'
     end
   end
 
-  def initialize(request)
-    @request = request
+  def initialize(data)
+    @data = data
   end
 
   def call
-    return Result.new([convert_user_format]) if valid_input_data? && acceptably?
-    return Result.new(["Unknown time format #{unknown_time_format}"]) if contain_unknown_params?
-    Result.new(["Undefined params #{@request.params.keys}"])
+    if !@data.nil?
+      return Result.new(convert_user_format) if valid_input_data?
+
+      Result.new("Unknown time format #{unknown_time_format}")
+    else
+      Result.new('Invalid param name')
+    end
   end
 
   private
 
   def valid_input_data?
-    correct_param_name? && @request.get?
-  end
-
-  def contain_unknown_params?
-    valid_input_data? && unknown_time_format.any?
-  end
-
-  def correct_param_name?
-    !@request.params["format"].nil?
-  end
-
-  def acceptably?
-    (@request.params["format"].split(",") - ACCEPTABLE_FORMAT).empty? if correct_param_name?
+    (@data.split(',') - TIME_CONVERT_FORMATS.keys).empty?
   end
 
   def unknown_time_format
-    @request.params["format"].split(",") - ACCEPTABLE_FORMAT
+    @data.split(',') - TIME_CONVERT_FORMATS.keys
   end
 
   def convert_user_format
-    display_format = @request.params["format"].split(",").reduce("") do |sum, param|
+    display_format = @data.split(',').reduce('') do |sum, param|
       "#{sum}#{TIME_CONVERT_FORMATS[param]}-"
     end.chop
 
